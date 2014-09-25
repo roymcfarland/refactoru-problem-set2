943eeb0b12863a25fb4b50566482718b4b3b2a2d
Roy McFarland <roy.mcfarland@gmail.com>
2014-09-25 17:37:39 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+///////////////////////////////
+////////// PROBLEM 1 //////////
+///////////////////////////////
+
+// var firstReverse = function(str) {
+// 	return str.split('').reverse().join('');
+// };
+// console.log( firstReverse('rhombus') );
+// console.log( firstReverse('racecar') );
+// console.log( firstReverse('zebra') );
+
+
+
+///////////////////////////////
+////////// PROBLEM 2 //////////
+///////////////////////////////
+
+var swapCase = function(str) {
+    var newArr = [];
+    for (var i=0; i<str.length; i++) {
+        if (str.charAt(i) === str.charAt(i).toLowerCase()) {
+            newArr.push(str.charAt(i).toUpperCase());
+        	} 
+        else if (str.charAt(i) === str.charAt(i).toUpperCase()) {
+            newArr.push(str.charAt(i).toLowerCase());
+        	}
+    };
+    return newArr.join(', ');
+};
+console.log( swapCase('Hello World') );
+console.log( swapCase('rOCK yoUr SoCKs ofF'));
