d6ecc3b0954c141956111ba42564da61c0db11b1
Roy McFarland <roy.mcfarland@gmail.com>
2014-09-26 11:06:50 -0600

    added setup to bonus question

--- main.js
+++ main.js
@@ -15,17 +15,38 @@
 ////////// PROBLEM 2 //////////
 ///////////////////////////////
 
-var swapCase = function(str) {
-    var newArr = [];
+// var swapCase = function(str) {
+//     var newArr = [];
+//     for (var i=0; i<str.length; i++) {
+//         if (str.charAt(i) === str.charAt(i).toLowerCase()) {
+//             newArr.push(str.charAt(i).toUpperCase());
+//         	} 
+//         else if (str.charAt(i) === str.charAt(i).toUpperCase()) {
+//             newArr.push(str.charAt(i).toLowerCase());
+//         	}
+//     };
+//     return newArr.join(', ');
+// };
+// console.log( swapCase('Hello World') );
+// console.log( swapCase('rOCK yoUr SoCKs ofF'));
+
+
+
+///////////////////////////////
+//////////// BONUS ////////////
+///////////////////////////////
+
+var letterCount = function(str) {
+    var newObj = {};
+    var part1 = str.split(' ');
+    
+    console.log(part1);
+
     for (var i=0; i<str.length; i++) {
-        if (str.charAt(i) === str.charAt(i).toLowerCase()) {
-            newArr.push(str.charAt(i).toUpperCase());
-        	} 
-        else if (str.charAt(i) === str.charAt(i).toUpperCase()) {
-            newArr.push(str.charAt(i).toLowerCase());
-        	}
-    };
-    return newArr.join(', ');
+        
+    }
+
+    console.log(newObj)
+
 };
-console.log( swapCase('Hello World') );
-console.log( swapCase('rOCK yoUr SoCKs ofF'));
+console.log( letterCount('Today is the greatest day ever!') );
